c5204e29087c324d79a616ffaeb090146b1b8b48
Fish <kenandcrys@live.com>
2024-10-23 12:32:20 -0500

    Something new

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+// DO NOT CHANGE THIS FILE
 const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
@@ -10,15 +11,15 @@ const sharedConfig = {
   // this enables foreign keys in SQLite
   pool: {
     afterCreate: (conn, done) => {
-      conn.run('PRAGMA foreign_keys = ON', done);
+      conn.run('PRAGMA foreign_keys = ON', done)
     },
   },
-};
+}
 
 module.exports = {
   development: {
     ...sharedConfig,
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: { filename: './data/Feed.db3' },
   },
   testing: {
@@ -26,14 +27,20 @@ module.exports = {
     connection: { filename: './data/testing.db3' },
   },
   production: {
-    // Use Sequelize in production for Postgres, Knex is not required for queries
-    useSequelize: true, // Custom flag to indicate Sequelize is used in production
-    // Migrations and Seeds directories can still be managed by Knex in production
-    migrations: {
-      directory: './data/migrations', // Knex can still manage migrations
+    client: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     },
-    seeds: {
-      directory: './data/seeds', // Knex can still manage seeds
-    },
-  },
+    connection: {
+      host: 'dpg-ckgt314ldqrs73f6u3gg-a',
+      port: 5432,
+      database: 'dev_i23c',
+      user: 'dev_i23c_user',
+      password: 'T5ar6BqpN1aovMDS5s0DO2LhLHZrWHCQ',
+    }
+  }
 };
